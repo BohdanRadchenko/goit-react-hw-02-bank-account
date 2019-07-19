4624ea4159fd4ebc3cf5efccd0ae9d4ec03af60a
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-19 08:11:05 +0300

    create react app

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+/*eslint-disable*/
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>App</p>
+      </div>
+    );
+  }
+}
+
+export default App;
